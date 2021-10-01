d2e431fd09ffdb752f2e6fa926770a3827e6b4bc
zarbabi <z.arbabi4@gmail.com>
2021-10-01 16:07:44 +0330

    delete item and update state

--- src/components/FullComment/FullComment.js
+++ src/components/FullComment/FullComment.js
@@ -12,8 +12,13 @@ const FullComment = ({ commentId }) => {
         .catch();
     }
   }, [commentId]);
-  
 
+  const deleteHandler = () => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
+  };
   let commentDetail = <p>please select a comment!</p>;
 
   if (commentId) commentDetail = <p>loading ...</p>;
@@ -24,6 +29,7 @@ const FullComment = ({ commentId }) => {
         <p>{comment.name}</p>
         <p>{comment.email}</p>
         <p>{comment.body}</p>
+        <button onClick={deleteHandler}>Delete</button>
       </div>
     );
   }
