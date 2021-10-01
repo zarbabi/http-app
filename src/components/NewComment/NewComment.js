350ea96271083265bfce5e7057207038f0b7ea05
zarbabi <z.arbabi4@gmail.com>
2021-10-01 13:57:18 +0330

    pos -new comment

--- src/components/NewComment/NewComment.js
+++ src/components/NewComment/NewComment.js
@@ -1,19 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
 import "./newComment.css";
+
 const NewComment = () => {
+  const [comment, setComment] = useState({ name: "", email: "", content: "" });
+
+  const nameHandler = (e) => {
+    setComment({ ...comment, name: e.target.value });
+  };
+
+  const emailHandler = (e) => {
+    setComment({ ...comment, email: e.target.value });
+  };
+
+  const contentHandler = (e) => {
+    setComment({ ...comment, content: e.target.value });
+  };
+
+  const postCommentHandler = (e) => {
+    axios
+      .post("https://jsonplaceholder.typicode.com/comments", comment)
+      .then((res) => console.log(res.data))
+      .catch();
+  };
+
   return (
     <div className="newComment">
       <div>
         <label>name</label>
-        <input type="text" />
+        <input type="text" onChange={nameHandler} />
       </div>
       <div>
         <label>email</label>
-        <input type="email" />
+        <input type="email" onChange={emailHandler} />
       </div>
       <div>
         <label>body</label>
-        <input type="textarea" />
+        <textarea type="textarea" onChange={contentHandler} />
       </div>
+      <button onClick={postCommentHandler}>add new comment</button>
     </div>
   );
 };
