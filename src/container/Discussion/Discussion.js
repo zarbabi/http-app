8cc4a2b1a5630c88e61a8d418ffe424e0134c4cf
zarbabi <z.arbabi4@gmail.com>
2021-10-01 20:51:57 +0330

    error handing conditional render with loafding and data

--- src/container/Discussion/Discussion.js
+++ src/container/Discussion/Discussion.js
@@ -7,17 +7,19 @@ import { useEffect, useState } from "react";
 const Discussion = () => {
   const [comments, setComments] = useState(null);
   const [selectedId, setSelectedId] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     //const getComments = async () =>
-    async function getComments() {
+    const getComments = async () => {
       try {
         const { data } = await axios.get("http://localhost:3001/comments");
         setComments(data);
       } catch (error) {
         console.log(error);
+        setError(true);
       }
-    }
+    };
     getComments();
   }, []);
 
@@ -25,22 +27,26 @@ const Discussion = () => {
     setSelectedId(id);
   };
 
+  const renderComments = () => {
+    let renderValue = <p>Loading ...</p>;
+
+    if (error) renderValue = <p>fetching data failed !</p>;
+
+    if (comments && !error) {
+      renderValue = comments.map((c) => (
+        <Comment
+          key={c.id}
+          name={c.name}
+          email={c.email}
+          onClick={() => selectCommentHandler(c.id)}
+        />
+      ));
+    }
+    return renderValue;
+  };
   return (
     <main>
-      <section>
-        {comments ? (
-          comments.map((c) => (
-            <Comment
-              key={c.id}
-              name={c.name}
-              email={c.email}
-              onClick={() => selectCommentHandler(c.id)}
-            />
-          ))
-        ) : (
-          <p>loading ...</p>
-        )}
-      </section>
+      <section>{renderComments()}</section>
       <section>
         <FullComment commentId={selectedId} />
       </section>
